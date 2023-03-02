f9aa2afccc444bd46ae18d618f05c89ecfd6a4a5
Mason Garrison <garrissm@wfu.edu>
2023-03-02 15:02:53 -0500

    clean up

--- data_prep.R
+++ data_prep.R
@@ -6,21 +6,6 @@ df_links <- read.csv("data/siblinks.csv", sep = ",")
 df_p2018_c <- read_sas("data/cfps2018person_202012.sas7bdat")
 df_c2018_c <- read_sas("data/cfps2018childproxy_202012.sas7bdat")
 
-# functions
-prettify_regression_results <- function(regression_object) {
-    regression_object %>%
-        gtsummary::tbl_regression(intercept=TRUE) %>%
-        gtsummary::add_glance_source_note(
-            label = list(statistic ~ "F-statistic",
-                         df  ~ "DF1",
-                         df.residual  ~ "DF2"),
-            include = c(r.squared, statistic, df, df.residual, p.value, nobs)
-        ) %>%
-        gtsummary::modify_header(
-            statistic ~ "**t-statistic**", p.value ~ "**p-value**"
-        )
-}
-
 
 #data cleaning
 
